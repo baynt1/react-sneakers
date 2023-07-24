c3880e0a530f2ccab8636550de1e72ce6f74bc46
baynt1 <algafura@gmail.com>
2023-07-25 01:05:04 +0600

    feat: app

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,23 @@
 import { FC } from "react";
+import { IHeader } from "../interfaces/interfaces";
 
-export const Header: FC = () => {
+export const Header: FC<IHeader> = ({ onOpenModal }) => {
   return (
     <header className="d-flex justify-between align-center p-40">
       <div className="d-flex align-center">
-        <img width={40} height={40} src="../../public/logo.png" alt={"logo"} />
+        <img width={40} height={40} src="/logo.png" alt={"logo"} />
         <div>
           <h3 className="text-uppercase">React Sneakers</h3>
           <p className="opacity-5">Магазин лучших кроссовок</p>
         </div>
       </div>
       <ul className="d-flex align-center">
-        <li className="mr-30">
-          <img
-            width={18}
-            height={18}
-            src="../../public/cart.svg"
-            alt={"cart"}
-          />
+        <li className="mr-30 cu-p" onClick={onOpenModal}>
+          <img width={18} height={18} src="/cart.svg" alt={"cart"} />
           <span>1205 руб.</span>
         </li>
         <li>
-          <img
-            width={18}
-            height={18}
-            src="../../public/user.svg"
-            alt={"user"}
-          />
+          <img width={18} height={18} src="/user.svg" alt={"user"} />
         </li>
       </ul>
     </header>
