719e329d25c698600b4b77eef03df31534e5dd12
baynt1 <algafura@gmail.com>
2023-07-26 01:14:30 +0600

    feat: search and some fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Card } from "./components/Cards";
 import { Header } from "./components/Header";
 import { Drawer } from "./components/Drawler";
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import axios from "axios";
 import { IListProducts } from "./interfaces/interfaces";
 
@@ -9,7 +9,7 @@ export const App = () => {
   const [list, setList] = useState<IListProducts[]>([]);
   const [modal, setModal] = useState<boolean>(false);
   const [cart, setCart] = useState<IListProducts[]>([]);
-
+  const [search, setSearch] = useState<string>("");
   // --------------- GET LIST OF PRODUCTS ------------------- //
   useEffect(() => {
     axios
@@ -28,29 +28,53 @@ export const App = () => {
   const onAddToCart = (item: IListProducts) => {
     setCart((prevState) => [...prevState, item]);
   };
+
+  const onSearch = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value);
+  };
+
+  const onClearSearch = () => setSearch("");
+
   return (
     <div className="wrapper clear">
       {modal && <Drawer list={cart} onCloseModal={() => setModal(false)} />}
       <Header onOpenModal={() => setModal(true)} />
       <div className="content p-40">
         <div className="d-flex align-center justify-between mb-40">
-          <h1>Все кроссовки</h1>
+          <h1 className="cut-text" style={{ maxWidth: "70%" }}>
+            {search === "" ? "Все кроссовки" : `Поиск по запросу: "${search}"`}
+          </h1>
           <div className="search-block d-flex">
+            {search && (
+              <img
+                className="clear cu-p"
+                src="/btn-remove.svg"
+                alt="clear"
+                onClick={onClearSearch}
+              />
+            )}
             <img src="/search.svg" alt="Search" />
-            <input placeholder="Поиск..." />
+            <input placeholder="Поиск..." value={search} onChange={onSearch} />
           </div>
         </div>
 
         <div className="d-flex flex-wrap">
-          {list.map((obj, index) => (
-            <Card
-              key={index}
-              title={obj.title}
-              price={obj.price}
-              imageUrl={obj.imageUrl}
-              onPlus={onAddToCart}
-            />
-          ))}
+          {list
+            .filter((obj) =>
+              obj.title
+                .toLowerCase()
+                .replace(/\s+/g, " ")
+                .includes(search.toLowerCase().replace(/\s+/g, " ")),
+            )
+            .map((obj, index) => (
+              <Card
+                key={index}
+                title={obj.title}
+                price={obj.price}
+                imageUrl={obj.imageUrl}
+                onPlus={onAddToCart}
+              />
+            ))}
         </div>
       </div>
     </div>
